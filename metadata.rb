262dd700ae6193fc5f9d2d51c3e33e8c54fd3159
Steffen Gebert <steffen.gebert@typo3.org>
2017-03-17 10:02:15 +0100

    [TESTS] Pipeline v2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@typo3.org"
 license          "Apache 2.0"
 description      "Skeleton Cookbook for TYPO3 Site Cookbooks"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.0.0"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 supports         "debian"
 
